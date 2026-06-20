5f2015e2901a6cc836e746d3d08425699252c222
Abdul Kasif <abdulkasif.ra@gmail.com>
2026-06-20 12:14:22 +0530

    proc: kern_vault: introduce proc entry skeleton
    Create a /proc entry named "kern_vault" during module initialization
    and clean it up upon module extraction. This establishes the basic
    filesystem node infrastructure required for userspace interactions.
    
    Currently, the 'proc_ops' structure is empty and will be populated
    with custom read and write handlers in subsequent patches. Safe error
    handling is included to abort module loading if creation fails.
    
    Signed-off-by: Abdul Kasif <abdulkasif.ra@gmail.com>

--- kern_vault/kern_vault.c
+++ kern_vault/kern_vault.c
@@ -1,17 +1,43 @@
+#include <linux/errno.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Abdul Kasif <abdulkasif.ra@gmail.com>");
 MODULE_DESCRIPTION("Write and read secret message in kernel");
 
+static struct proc_dir_entry *kern_vault_dir_entry;
+
+static const struct proc_ops kern_vault_proc_ops = {};
+
 static int __init kern_vault_init(void) {
-  pr_info("kern_vault loaded!\n");
+  pr_info("kern_vault init: entry\n");
+
+  kern_vault_dir_entry =
+      proc_create("kern_vault", 0444, NULL, &kern_vault_proc_ops);
+
+  if (kern_vault_dir_entry == NULL) {
+    pr_err("Failed to create /proc/kern_vault entry\n");
+    return -ENOMEM;
+  }
+
+  pr_info("kern_vault init: exit\n");
+
   return 0;
 }
 
-static void __exit kern_vault_exit(void) { pr_info("kern_vault removed!\n"); }
+static void __exit kern_vault_exit(void) {
+
+  pr_info("kern_vault exit: entry\n");
+
+  if (kern_vault_dir_entry) {
+    proc_remove(kern_vault_dir_entry);
+  }
+
+  pr_info("kern_vault exit: exit\n");
+}
 
 module_init(kern_vault_init);
 module_exit(kern_vault_exit);
