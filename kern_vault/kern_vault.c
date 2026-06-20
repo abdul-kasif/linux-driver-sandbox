72af84ce23b5193a1be5b060d4f35978b7f2f78d
Abdul Kasif <abdulkasif.ra@gmail.com>
2026-06-20 12:38:28 +0530

    proc: kern_vault: implement basic read and write handlers
    Define empty read and write callback operations for the /proc entry
    to prevent kernel panic during user access. Wire these handlers into
    the 'proc_ops' structure using the standard .proc_read and .proc_write
    function pointers.
    
    Additionally, allocate a static 128-byte array buffer for the secret
    message storage along with an atomic transaction counter variable to
    track active operations securely in subsequent patches.
    
    Signed-off-by: Abdul Kasif <abdulkasif.ra@gmail.com>

--- kern_vault/kern_vault.c
+++ kern_vault/kern_vault.c
@@ -1,16 +1,38 @@
+#include <linux/atomic.h>
 #include <linux/errno.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/proc_fs.h>
+#include <linux/types.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Abdul Kasif <abdulkasif.ra@gmail.com>");
 MODULE_DESCRIPTION("Write and read secret message in kernel");
 
+static char secret_message[128];
+
+static atomic_t message_counter = ATOMIC_INIT(0);
+
+static ssize_t kern_vault_proc_read(struct file *file, char __user *user_buf,
+                                    size_t count, loff_t *ppos) {
+  pr_info("kern_vault: inside kern_vault_proc_read.\n");
+  return 0;
+}
+
+static ssize_t kern_vault_proc_write(struct file *file,
+                                     const char __user *user_buf, size_t count,
+                                     loff_t *ppos) {
+  pr_info("kern_vault: inside kern_vault_proc_write.\n");
+  return -1;
+}
+
 static struct proc_dir_entry *kern_vault_dir_entry;
 
-static const struct proc_ops kern_vault_proc_ops = {};
+static const struct proc_ops kern_vault_proc_ops = {
+    .proc_read = kern_vault_proc_read,
+    .proc_write = kern_vault_proc_write,
+};
 
 static int __init kern_vault_init(void) {
   pr_info("kern_vault init: entry\n");
